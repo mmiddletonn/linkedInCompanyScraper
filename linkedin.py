2a2489bbc56bc00d6984fe44fc8a17ee2f536669
Matthew Middleton <matth@matthews-mbp-2.lan>
2024-03-11 17:34:23 -0400

    beta 2

--- linkedin.py
+++ linkedin.py
@@ -12,24 +12,17 @@ class session:
         self.wait()
 
     def init_driver(self, ostype):
-        # Validate the OS type argument
         if not ostype:
             raise ValueError("An OS type must be specified.")
 
-        # Get the directory of the current script
         script_directory = os.path.dirname(os.path.realpath(__file__))
 
-        # Construct the path to ChromeDriver dynamically based on the OS type
         chrome_driver_path = os.path.join(script_directory, f'chromedriver-{ostype}', 'chromedriver')
 
-        # Set up the ChromeDriver options
         options = webdriver.ChromeOptions()
-        # Add any Chrome options if needed
 
-        # Set up the service with the dynamically constructed path
         service = Service(executable_path=chrome_driver_path)
 
-        # Initialize the WebDriver instance using the specified ChromeDriver
         self.driver = webdriver.Chrome(service=service, options=options)
 
     def login(self):
@@ -38,8 +31,7 @@ class session:
             raise Exception("Driver has not been initialized.")
         self.driver.get(url)
 
-
-    def scrape(self, query, max_pages=2):
+    def scrape(self, query, max_pages=1):
         base_url = "https://www.linkedin.com/search/results/people/?keywords="
         if self.driver is None:
             raise Exception("Driver has not been initialized.")
@@ -71,7 +63,6 @@ class session:
         else:
             unique_email_addresses = list(set(all_email_addresses))
             return str(unique_email_addresses)
-
 
     def wait(self):
         input("Press enter to continue...")
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+import csv
 import os_detection
 import linkedin
 
 platform = os_detection.platform
 session = linkedin.session(platform)
 
-companies = ["google", "apple"]
 employee_emails = {}
 
-for company in companies:
-    employee_emails[f"{company}"] = session.scrape(f"{company}")
+with open('data.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        for company in row:
+            # print(company)
+            employee_emails[company] = session.scrape(company)
 
 print(employee_emails)
 
